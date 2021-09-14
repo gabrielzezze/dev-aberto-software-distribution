9de3e58434621dbd29def188db16014d50304911
Gabriel Zezze <gabrielfz@al.insper.edu.br>
2021-09-14 18:51:43 -0300

    Deployment to PyPi done

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="dev_aberto_gabrielzezze",
+    version="0.1.2",
+    packages=["dev_aberto"],
+    author="Gabriel Zezze",
+    author_email="gabriel@zezze.dev",
+    description="Example package for Open development class by Insper",
+    long_description="Example package for Open development class by Insper",
+    url="https://github.com/gabrielzezze/dev-aberto-software-distribution",
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    python_requires=">=3.6",
+    install_requires=["requests"],
+    scripts=["scripts/hello.py"],
+)
